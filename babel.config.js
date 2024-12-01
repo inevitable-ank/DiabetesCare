d1d4588cbb5da0f1c85a0840ed270c091062f395
inevitable-ank <ankitranjanyadav@gmail.com>
2024-12-01 13:16:27 +0530

    Deploy

--- babel.config.js
+++ babel.config.js
@@ -1,12 +1,4 @@
 module.exports = {
-  presets: [
-    "@babel/preset-env",
-    [
-      "@babel/preset-react",
-      {
-        runtime: "classic",
-      },
-    ],
-  ],
+  "presets": [ "@babel/preset-env", ["@babel/preset-react", { "runtime": "automatic" }] ]
 };
 
